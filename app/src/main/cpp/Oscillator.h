9be2a47889d522a4a4da53f44fc8b40c3a5c3f22
ahleung2 <alvanguard128@gmail.com>
2023-07-17 21:16:31 -0700

    Declared new methods and variables in Oscillator header file

--- app/src/main/cpp/Oscillator.h
+++ app/src/main/cpp/Oscillator.h
@@ -13,11 +13,21 @@ public:
     void setSampleRate(int32_t sampleRate);
     void render(float *audioData, int32_t numFrames);
 
+    // add a new method that sets the frequency according to input
+    void setFrequency(float frequency);
+
 private:
     std::atomic<bool> isWaveOn_{false};
     double phase_ = 0.0;
     double phaseIncrement_ = 0.0;
 
+    // set default value for frequency_ and sampleRate_
+    // 44100 is the standard sample rate
+    float frequency_ = 440.0;
+    int32_t sampleRate_ = 44100;
+
+    // create method that will calculate the new phase increment based on frequency
+    void calcPhaseIncrement();
 };
 
 
